a81a81e27390d55f930aba19ddcb92d9fa89c2ab
DivonilLiquid <mait.madhur@gmail.com>
2021-06-01 21:13:10 +0530

    First trial

---
+++ script.js
@@ -0,0 +1,36 @@
+var typed = new Typed(".changeSyntax",{
+    strings: ["a student","an undergraduate","a Software Developer","a Tutor","a Youtuber"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".changeSyntaxLeetcode",{
+    strings: ["3⭐️ Coder at leetcode","Have solved more then 300 problems on DSA"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+})
+var typed = new Typed(".changeSyntaxCodechef",{
+    strings: ["3⭐️ Coder at leetcode","Have solved more then 300 problems on DSA"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+})
+var typed = new Typed(".changeSyntaxlHackerrank",{
+    strings: ["a student","an undergraduate","a Software Developer","a Tutor","a Youtuber"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+})
+var typed = new Typed(".changeSyntaxSpoj",{
+    strings: ["a student","an undergraduate","a Software Developer","a Tutor","a Youtuber"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+})
+var typed = new Typed(".changeSyntaxHackerblocks",{
+    strings: ["a student","an undergraduate","a Software Developer","a Tutor","a Youtuber"],
+    typeSpeed: 30,
+    backSpeed: 10,
+    loop: true
+})
